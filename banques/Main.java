ca00b883d02b6bb8f327b059e0aa18a1c707c721
mouctare <mouctard78@gmail.com>
2021-12-28 12:51:51 +0100

    Simulation d'un compte courant

--- banques/Main.java
+++ banques/Main.java
@@ -18,7 +18,47 @@ public  static void main(String[] args) {
 
     // recolter ce que la personne va ecrire
     Scanner scanner = new Scanner(System.in);
+      
     int choix = scanner.nextInt();
+
+    // verification
+    if(choix == 1){
+        System.out.println("Votre solde est de: " + compteCourant);
+    }
+
+    if(choix == 2){
+        System.out.println("Combien souhaitez vous déposer sur votre compe ? ");
+
+        // recuperer le montant 
+
+        int montant = scanner.nextInt();
+
+        // ajouter au solde/compte courant
+         compteCourant += montant;
+
+         // un messange qui confirme que le depot s'est bien fait
+
+         System.out.println("Vous avez déposez  " + montant + " euro ");
+
+    }   if(choix == 3){
+        System.out.println("Combien souhaitez vous retirez d'argent  sur votre compte ?");
+
+        int montant = scanner.nextInt();
+
+         // vérifier si elle a assez d'argent
+
+         if(compteCourant - montant >= 0){
+
+            compteCourant -= montant;
+
+            System.out.println("Vous avez récuperer  " + montant + " euro ");
+            System.out.println("Votre solde est de  " + compteCourant + " euro ");
+
+         } else{
+            System.out.println("Vous n'avez pas les fonds nécessaires");
+ 
+         }
+    }
 
 }
     

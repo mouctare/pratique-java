bc38d07ba092da51cad2a0b12710d371e3afa08a
mouctare <mouctard78@gmail.com>
2021-12-29 14:29:03 +0100

    suite pratique condition

--- adn/Main.java
+++ adn/Main.java
@@ -22,25 +22,33 @@ public static void main(String[] args) {
           System.out.println("Bonjour, " + prenom + ", vous avez " + age + " ans" + " vous etes du genre "  + sexe);
     
          
-          String adn = "77777x7777x777x77x77xxxx";
+          String adn = "77x7xx7";
 
           long compteurTaille = adn.chars().filter(c -> c == 'x').count();
           System.out.println("nombre de x : " + compteurTaille);
       
 
-        if(compteurTaille < 18 && age < 21){
+        if(compteurTaille == 1){
 
-        System.out.println(" vous avez moins  de 21 ans");
+        System.out.println(" 0.5m-1m50");
      }
-       else if(compteurTaille > 6 && age > 21){
-        System.out.println(" vrai");
+       else if(compteurTaille == 2){
+        System.out.println(" 1m51-1m70");
 
      }
-       else if(compteurTaille <= 6 && age >= 65){
-        System.out.println(" vrai");
+       else if(compteurTaille == 3){
+        System.out.println("1m71-1m80");
+
+    }
+       else if(compteurTaille == 4){
+        System.out.println("1m81-1m90");
+     }
+    
+       else if(compteurTaille == 5){
+        System.out.println("+ 1m90");
 
      }else{
-        System.out.println("faux, vous mentez");  
+        System.out.println("plus de 2m");  
      }
          
     } 

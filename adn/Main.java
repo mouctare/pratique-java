10927ccc98475991a8d38cc3585e0c966ad654d9
mouctare <mouctard78@gmail.com>
2021-12-29 11:55:51 +0100

    l'usage du scaner

--- adn/Main.java
+++ adn/Main.java
@@ -1,16 +1,28 @@
 package adn;
 
+import java.util.Scanner;
+
 public class Main {
 
 public static void main(String[] args) {
 
-    String adn = "999";
-     
-    //Vérifier qu'un caractère est présent dans variable tableau ou une chaine
-    long count = adn.chars().filter(c -> c == '7').count();
-        String phrase = count == 0 ? "La personne est un homme" : "La personne set une femme";
-  
-     System.out.println(phrase);
+    Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Bonjour Quel est votre prenom");
+
+      String prenom = scanner.nextLine();
+
+      System.out.println("Quel est votre age");
+
+      int age = scanner.nextInt();
+
+      System.out.println("Quel est votre genre/sexe");
+
+      String sexe = scanner.next();
+
+      System.out.println("Bonjour, " + prenom + ", vous avez " + age + " ans" + " vous etes du genre "  + sexe);
+         
+         
     } 
     
   }

25d275337fecc6c78d8cfb20ac8811912fbe1692
mouctare <mouctard78@gmail.com>
2021-12-29 16:20:22 +0100

    lire un fichier

--- adn/Main.java
+++ adn/Main.java
@@ -1,56 +1,44 @@
 package adn;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 public static void main(String[] args) {
 
-    try (Scanner scanner = new Scanner(System.in)) {
-        System.out.println("Bonjour Quel est votre prenom");
+    // parcourir le fichier et lire chaque sequence d'ADN
+   File monfier = new File("nav.txt");
 
-          String prenom = scanner.nextLine();
+   // verifier que le ficier existe
+   if(monfier.exists()) {
+       System.out.println("Fier existant");
 
-          System.out.println("Quel est votre age");
+       // on essaie de Lire Le fichier
 
-          int age = scanner.nextInt();
+    try{
+       
+        BufferedReader lecture = new BufferedReader(new FileReader(monfier));
+        String ligne = lecture.readLine();
 
-          System.out.println("Quel est votre genre/sexe");
-
-          String sexe = scanner.next();
-
-          System.out.println("Bonjour, " + prenom + ", vous avez " + age + " ans" + " vous etes du genre "  + sexe);
-    
-         
-          String adn = "77x7xx7";
-
-          long compteurTaille = adn.chars().filter(c -> c == 'x').count();
-          System.out.println("nombre de x : " + compteurTaille);
-
-        switch(Math.toIntExact(compteurTaille)){
-            case 1:
-            System.out.println("0.5m-1m50");
-              break;
-            case 2:
-            System.out.println("1m51-1m70");
-             break;
-            
-            case 3:
-            System.out.println("1m71-1m80");
-             break;
-            case 4:
-            System.out.println("1m81-1m90");
-             break;
-
-            case 5:
-            System.out.println("+ 1m90");
-             break;
-            default:
-              System.out.println("Taille non trouvé !");
-              break;
+        // boucler sur chaque ligne tant qu'il y'a en a encore
+        while(ligne != null){
+            System.out.println(ligne);
+            ligne = lecture.readLine();
         }
 
-    }
+
+       }catch(IOException e){
+         e.printStackTrace();
+       }
+     
+      
+   }else{
+    System.out.println("Fier existant not found");  
+   }
 }
 
 }

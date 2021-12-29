17db6f5b90c24fb4c55c2cfb2e4297fb0b14cb7b
mouctare <mouctard78@gmail.com>
2021-12-29 11:39:52 +0100

    utilisation de la ternaire

--- adn/Main.java
+++ adn/Main.java
@@ -8,23 +8,10 @@ public static void main(String[] args) {
      
     //Vérifier qu'un caractère est présent dans variable tableau ou une chaine
     long count = adn.chars().filter(c -> c == '7').count();
-    int nombreCaracteres = adn.length();
-    
-
-    if(nombreCaracteres <= 18){
-        System.out.println("Vous avez moins de 21 ans");
-    } else if(nombreCaracteres == 21 && nombreCaracteres > 65){
-        System.out.println("Vous avez  21 ans");
-    }else{
-        System.out.println("Vous avez plus de 65 ans");
-    }
-
-     if(count == 0){
-        System.out.println("Le sexe de la personne est une femme");
-    }else{
-     System.out.println("Le sexe de la personne est un homme ");
+        String phrase = count == 0 ? "La personne est un homme" : "La personne set une femme";
+  
+     System.out.println(phrase);
     } 
     
   }
 
-}

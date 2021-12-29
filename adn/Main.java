7a6cec6e9b5f3090253d3d2315b95e725bc4299d
mouctare <mouctard78@gmail.com>
2021-12-29 13:45:58 +0100

    controle de l'age via l'adn

--- adn/Main.java
+++ adn/Main.java
@@ -6,24 +6,43 @@ public class Main {
 
 public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Bonjour Quel est votre prenom");
 
-     System.out.println("Bonjour Quel est votre prenom");
+          String prenom = scanner.nextLine();
 
-      String prenom = scanner.nextLine();
+          System.out.println("Quel est votre age");
 
-      System.out.println("Quel est votre age");
+          int age = scanner.nextInt();
 
-      int age = scanner.nextInt();
+          System.out.println("Quel est votre genre/sexe");
 
-      System.out.println("Quel est votre genre/sexe");
+          String sexe = scanner.next();
 
-      String sexe = scanner.next();
-
-      System.out.println("Bonjour, " + prenom + ", vous avez " + age + " ans" + " vous etes du genre "  + sexe);
+          System.out.println("Bonjour, " + prenom + ", vous avez " + age + " ans" + " vous etes du genre "  + sexe);
+    
          
+        String adn = "77x77x7777x777x777x777";
+        int compteCaractere = adn.length();
+
+        if(compteCaractere < 18 && age >= 21){
+
+        System.out.println(" + de 21 ans");
+     }
+       else if(compteCaractere > 6 && age <= 21){
+        System.out.println(" vrai");
+
+     }
+       else if(compteCaractere <= 6 && age >= 65){
+        System.out.println(" vrai");
+
+     }else{
+        System.out.println("faux, vous mentez");  
+     }
          
     } 
     
-  }
+}
+
+}
 

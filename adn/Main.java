352127bea51453e0037ccc73e65ef3f5e4b39229
mouctare <mouctard78@gmail.com>
2021-12-29 11:29:35 +0100

    utilisation des lamda  conditions

--- adn/Main.java
+++ adn/Main.java
@@ -4,33 +4,27 @@ public class Main {
 
 public static void main(String[] args) {
 
-    String adn = "7777x777777x777777";
-  
+    String adn = "999";
+     
+    //Vérifier qu'un caractère est présent dans variable tableau ou une chaine
+    long count = adn.chars().filter(c -> c == '7').count();
+    int nombreCaracteres = adn.length();
+    
 
-    // analyser si c'est de 7 ou des 9
-
-    char[] tableau = adn.toCharArray();
-    String sexe = "femme";
-
-    if(adn.contains("7")){
-        sexe = "homme";
-       }
-
-    System.out.println("Le sexe de la personne est: " + sexe);
-
-
-  /*   for(char caractere : tableau){
-        System.out.println(caractere);
-
-        if(caractere == '7'){
-            sexe = "homme";
-        }
-
-        System.out.println("Le sexe de la personne est: " + sexe);
+    if(nombreCaracteres <= 18){
+        System.out.println("Vous avez moins de 21 ans");
+    } else if(nombreCaracteres == 21 && nombreCaracteres > 65){
+        System.out.println("Vous avez  21 ans");
+    }else{
+        System.out.println("Vous avez plus de 65 ans");
     }
 
-     }
-      */
-    }
+     if(count == 0){
+        System.out.println("Le sexe de la personne est une femme");
+    }else{
+     System.out.println("Le sexe de la personne est un homme ");
+    } 
+    
+  }
 
 }
